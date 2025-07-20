0bde4fe3bd04a68ea14756e0d30d85414986d60f
kingsleyrhema <kingsleyrhema135@gmail.com>
2025-07-20 23:09:47 +0100

    add to cart WO1

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,13 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  // Retrieve current cart or initialize as empty array
+  let cart = getLocalStorage("so-cart") || [];
+  cart.push(product);
+  setLocalStorage("so-cart", cart);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
